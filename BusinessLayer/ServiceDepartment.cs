75d39eadc65d3410ea97bdb8d091f3002c3d1d86
Thendo201 <578365@student.belgiumcampus.ac.za>
2023-10-02 21:01:01 +0200

    changed some stuff

--- BusinessLayer/ServiceDepartment.cs
+++ BusinessLayer/ServiceDepartment.cs
@@ -4,29 +4,29 @@ public class ServiceDepartment
 {
 	public ServiceDepartment()
 	{
-		static void answer()
+		public void answer()
 		{
             //code to anser incoming service calls 
 		}
-        static void viewServiceProgress()
+        public void viewServiceProgress()
         {
             //Code to be able to view the progress of services offered to a client
         }
-        static void recieveServiceRequest()
+        public void recieveServiceRequest()
         {
             //Code to enable premier solutions to recieve service requests from clients
         }
-        static string trackJob(string job)
+        public string trackJob(string job)
         {
             //code to be able to track all jobs and see their current status
             return job;
         }
-        static string ReassignJob(string job)
+        public string ReassignJob(string job)
         {
             //Code to be able to reasign jobs
             return job;
         }
-        static void closeServiceRequest(string serviceRequest)
+        public void closeServiceRequest(string serviceRequest)
         {
             //code to close a service request either due to completion or cancelation
         }
