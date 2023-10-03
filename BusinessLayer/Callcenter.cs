435f66d47265fa55d8c56d67386e5ce8dcb498c4
HenkoSteyn <91952738+HenkoSteyn@users.noreply.github.com>
2023-10-03 11:09:47 +0200

    added callcenter

---
+++ BusinessLayer/Callcenter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class CallCenter
+{
+	public CallCenter()
+	{
+
+        public string trackJob(string job)
+        {
+            //code to be able to track all jobs and see their current status
+            return job;
+        }
+
+        public void answercall()
+        {
+            //code to anser incoming service calls 
+        }
+
+        public Client GetClientData(int clientId)
+        {
+            // retrieve client data code
+            return null;
+        }
+
+        public void ViewCustomerDetails(int clientId)
+        {
+            // view customer details code
+        }
+
+        public void SubmitWorkRequest(WorkRequest request)
+        {
+            //  workrequest submission code 
+        }
+    }
+}
