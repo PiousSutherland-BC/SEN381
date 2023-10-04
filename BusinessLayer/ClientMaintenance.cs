b08a0924ac9ccbe4086589984844eb00e3042834
Thendo201 <578365@student.belgiumcampus.ac.za>
2023-10-04 11:49:36 +0200

    I am pushing Justins code

---
+++ BusinessLayer/ClientMaintenance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class ClientMaintenance
+{
+	public ClientMaintenance()
+	{
+        @Override public void monitorPerformance()
+        {
+            //override code
+        }
+        @Override public void escalate()
+        {
+            //override code
+        }
+        string UncoverDetails(string Client)
+        {
+
+            return Client;
+        }
+        string DetailsLogging(string Client)
+        {
+
+            return Client;
+        }
+        string SubmitWorkRequest(string Client)
+        {
+
+            return Client;
+        }
+        void CloseRequest()
+        {
+            // close current request
+        }
+
+        void ViewPrevious()
+        {
+            // view previous maintenance report
+        }
+
+        void IdentifyExcSituations()
+        {
+            // Identify executed situations
+        }
+        void CaptureMaintainDetails()
+        {
+            // Capture and maintain details
+        }
+    }
+}
